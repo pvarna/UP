46c1acaa1d026fa84766568887711c2455baf882
pvarna <peter.kolev2001@gmail.com>
2020-12-07 11:20:24 +0200

    updated task3 of the first HW

--- Homeworks/1/task3.cpp
+++ Homeworks/1/task3.cpp
@@ -114,11 +114,7 @@ int main ()
 
 bool isYearLeap (int year)
 {
-    if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))
-    {
-        return true;
-    }
-    return false;
+    return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
 }
 
 bool isDateValid (int day, int month, int year)
